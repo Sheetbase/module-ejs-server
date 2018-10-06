0f2af5c99238bc362ed1bd8d3ea5a133b03c34b5
Lam Nhan <lamnhan92@gmail.com>
2018-10-06 16:57:20 +0700

    app-scripts 0.0.3

---
+++ src/example.ts
@@ -0,0 +1,9 @@
+declare const EjsModule: {()};
+var Ejs = Ejs || EjsModule();
+
+export function example1(): void {
+	const source: string = '<h1>Hello <%= name %>!</h1>';
+	const data = {name: 'world'};
+	let html: string = Ejs.render(source, data);
+	Logger.log(html);
+}
